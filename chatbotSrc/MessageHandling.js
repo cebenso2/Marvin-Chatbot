ac8e44d03d9893587e3a6e15f57ace4ee613dc0b
cebenso2 <cebenso2@illinois.edu>
2017-11-13 03:11:31 -0600

    nlp right thing

--- chatbotSrc/MessageHandling.js
+++ chatbotSrc/MessageHandling.js
@@ -9,7 +9,7 @@ function handleMessage(sender_psid, received_message) {
   let response;
   // Check if the message contains text
   if (received_message.text) {
-    let greeting = firstEntity(received_message.nlp, 'greeting');
+    let greeting = firstEntity(received_message, 'greeting');
     console.log(greeting);
     // Create the payload for a basic text message
     if (received_message.text === "@help") {
