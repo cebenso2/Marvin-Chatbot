7cd1f08cebf3e798afbc6a5ffbdaf4a882e4be95
cebenso2 <cebenso2@illinois.edu>
2017-11-13 02:09:10 -0600

    fetch

--- chatbotSrc/WeatherDataUtils.js
+++ chatbotSrc/WeatherDataUtils.js
@@ -1,12 +1,11 @@
 var request = require("request");
-var fetch = require("fetch");
 const WEATHER_ENDPOINT = "http://api.wunderground.com/api/" + process.env.WEATHER_API_KEY + "/conditions/q/"
 function getWeatherData(latitude, longitude) {
 
   let response;
 
   // Send the HTTP request to the Messenger Platform
-  fetch(WEATHER_ENDPOINT + latitude +","+longitude+".json").then(
+  fetch(WEATHER_ENDPOINT + latitude +","+longitude+".json",{method: 'GET',}).then(
     (response) => response.json()).then(
       (json) => {
         console.log(json.body.current_observation.feelslike_string)
