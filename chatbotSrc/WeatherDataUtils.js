623d45fd7666276bde527cdd488be9677e606b70
cebenso2 <cebenso2@illinois.edu>
2017-11-13 02:45:08 -0600

    finish weather and update index

--- chatbotSrc/WeatherDataUtils.js
+++ chatbotSrc/WeatherDataUtils.js
@@ -1,14 +1,11 @@
 var request = require("request-promise");
+
 const WEATHER_ENDPOINT = "http://api.wunderground.com/api/" + process.env.WEATHER_API_KEY + "/conditions/q/"
+
 function getWeatherData(latitude, longitude) {
-
-  let response;
-
-  // Send the HTTP request to the Messenger Platform
   return request(WEATHER_ENDPOINT + latitude +","+longitude+".json").then(
     response => {
       let data = JSON.parse(response);
-      //console.log(response.body.current_observation.feelslike_string);
       return data.current_observation.feelslike_string;
     }
   ).catch(error => console.log(error))
