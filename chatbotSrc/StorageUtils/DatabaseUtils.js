78b43bb30e9fcf71008f3a4b76cda026b14c9638
cebenso2 <cebenso2@illinois.edu>
2017-11-21 14:55:39 -0600

    asdfasdf

--- chatbotSrc/StorageUtils/DatabaseUtils.js
+++ chatbotSrc/StorageUtils/DatabaseUtils.js
@@ -10,10 +10,12 @@ function createDatabase(){
 
   client.connect();
   client.query("CREATE TABLE Locations (LocationName varchar(255),Latitude FLOAT,Longitude FLOAT);", (err, res) => {
+    console.log("test");
     console.log(err);
     console.log(res);
   });
   client.query('SELECT LocationName,Locations FROM information_schema.tables;', (err, res) => {
+    console.log("test 1");
     if (err) throw err;
     for (let row of res.rows) {
       console.log(JSON.stringify(row));
