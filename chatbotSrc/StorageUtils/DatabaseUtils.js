01d289550befac7089131b498e5cfc46898a8288
cebenso2 <cebenso2@illinois.edu>
2017-11-21 14:40:13 -0600

    string syntax?

--- chatbotSrc/StorageUtils/DatabaseUtils.js
+++ chatbotSrc/StorageUtils/DatabaseUtils.js
@@ -15,11 +15,7 @@ function createDatabase(){
       console.log(JSON.stringify(row));
     }
   });*/
-  client.query("CREATE TABLE Persons (
-    LocationName varchar(255),
-    Latitude FLOAT,
-    Longitude FLOAT
-  );", (err, res) => {
+  client.query("CREATE TABLE Persons (LocationName varchar(255),Latitude FLOAT,Longitude FLOAT);", (err, res) => {
     console.log(err);
     console.log(res);
   });
