f70634344b183985ee5f24aee15f91a47fb01977
cebenso2 <cebenso2@illinois.edu>
2017-11-27 10:10:24 -0600

    thing

--- chatbotSrc/DataUtils/SportsDataUtils.js
+++ chatbotSrc/DataUtils/SportsDataUtils.js
@@ -11,27 +11,30 @@ const SPORTS_SCHEDULE_ENDPOINT = "https://api.mysportsfeeds.com/v1.1/pull/nba/cu
 
 //returns the current temperature at the give lat and long
 function getTeamSchedule(team) {
-  /*let options = {
+  let options = {
     uri: SPORTS_SCHEDULE_ENDPOINT,
     headers: {
       'User-Agent': 'Request-Promise',
-      'Authentication': 'Basic ' + btoa(process.env.SPORTS_USERNAME +":"+process.env.SPORTS_PASSWORD),
+      'Authentication': 'Basic ' + (process.env.SPORTS_USERNAME +":"+process.env.SPORTS_PASSWORD),
     },
-    json: true // Automatically parses the JSON string in the response
+    method: 'GET',
+    qs: {},
+    transform: function(body) {
+      // console.log("response = '" + util.inspect(body, {depth: null}) + "'");
+      return body;
+    },
+    json: true,// Automatically parses the JSON string in the response
   };
   console.log(options);
   return request(options).then(
     response => {
-      let data = JSON.parse(response);
-      console.log(data);
-      return data;
+      console.log(reponse);
+      //let data = JSON.parse(response);
+      //console.log(data);
+      //return data;
     }
   ).catch(error => console.log(error))*/
 
-  let msf = new MySportsFeeds("1.0", true, null);
-  msf.authenticate(process.env.SPORTS_USERNAME, process.env.SPORTS_PASSWORD);
-  let data = msf.getData('nba', '2016-2017-regular', 'player_gamelogs', 'json', {player: 'stephen-curry'});
-  console.log(data);
 }
 
 module.exports = {getTeamSchedule: getTeamSchedule}
