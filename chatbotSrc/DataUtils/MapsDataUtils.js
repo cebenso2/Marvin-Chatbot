4866a2e72afd1a0a59315b7a97348d6a7f1534b9
cebenso2 <cebenso2@illinois.edu>
2017-12-04 02:03:31 -0600

    thing

--- chatbotSrc/DataUtils/MapsDataUtils.js
+++ chatbotSrc/DataUtils/MapsDataUtils.js
@@ -1,5 +1,5 @@
+var request = require("request-promise");
 let endpoint = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Victoria+BC&key=AIzaSyAYtdgKqjDivyTKd2CcHbN8h7P2TwWbotk"
-
 
 function getTimeFromOriginToDest() {
   return request(endpoint).then(
