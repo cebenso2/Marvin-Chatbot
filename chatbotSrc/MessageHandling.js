45ab6be9dd8404ce190f0ea3e3d166c13079f928
cebenso2 <cebenso2@illinois.edu>
2017-12-11 04:02:19 -0600

    ai

--- chatbotSrc/MessageHandling.js
+++ chatbotSrc/MessageHandling.js
@@ -96,7 +96,7 @@ function handleMessage(sender_psid, received_message) {
         "text": "Hello! My name is Marvin and I am good.",
       }
     } else {
-      Wit.processWithAI(sender_psid, received_message.text).then(result => {
+      Wit.processWithAI(received_message.text).then(result => {
         console.log(result);
       });
       return;
