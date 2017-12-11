cd3c42343f9c36a840fbe3af2085e195887e5dc5
cebenso2 <cebenso2@illinois.edu>
2017-12-11 05:05:50 -0600

    thing

--- chatbotSrc/Utils/tip.js
+++ chatbotSrc/Utils/tip.js
@@ -2,7 +2,7 @@ function createTipString(number){
     let string = "Here are tip results for $"+number+".\n " ;
     let tip = 10;
     for ( ; tip <21; tip +=5){
-      string+= tip +"% would be $"+number*tip/100+".\n ";
+      string+= tip +"% would be $"+Math.round(number*tip)/100+".\n ";
     }
     return string;
 }
