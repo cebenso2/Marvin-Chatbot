9f264aacca07bae5d2252740c3ce62a21266bb72
cebenso2 <cebenso2@illinois.edu>
2017-12-10 22:52:21 -0600

    change url?

--- chatbotSrc/Email/RetreiveToken.js
+++ chatbotSrc/Email/RetreiveToken.js
@@ -2,13 +2,13 @@
 
 var fs = require('fs');
 var googleAuth = require('google-auth-library');
-let client_secret = '{"web":{"client_id":"696019611227-5m62hp5vit4ossvpv7pemo92prrlric8.apps.googleusercontent.com","project_id":"marvinassistantc-1512373709006","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"Ce7jPpvZ7x4nUXJqnzr8DJzE","redirect_uris":["http://marvin-assistant.herokuapp.com/?email=bob"],"javascript_origins":["http://marvin-assistant.herokuapp.com"]}}'
+let client_secret = '{"web":{"client_id":"696019611227-5m62hp5vit4ossvpv7pemo92prrlric8.apps.googleusercontent.com","project_id":"marvinassistantc-1512373709006","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"Ce7jPpvZ7x4nUXJqnzr8DJzE","redirect_uris":["http://marvin-assistant.herokuapp.com/"],"javascript_origins":["http://marvin-assistant.herokuapp.com"]}}'
 
 
 //gets authorization token from google
 function getAuthorizationToken(code, cb) {
   // Load client secrets
-  var credentials = JSON.parse(client_secret);
+  var credentials = JSON.parse(data);
   var clientSecret = credentials.web.client_secret;
   var clientId = credentials.web.client_id;
   var redirectUrl = credentials.web.redirect_uris[0];
--- chatbotSrc/Email/RetreiveUrl.js
+++ chatbotSrc/Email/RetreiveUrl.js
@@ -3,7 +3,7 @@
 var fs = require('fs');
 var googleAuth = require('google-auth-library');
 var Scopes = require('./Scopes');
-let client_secret = '{"web":{"client_id":"696019611227-5m62hp5vit4ossvpv7pemo92prrlric8.apps.googleusercontent.com","project_id":"marvinassistantc-1512373709006","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"Ce7jPpvZ7x4nUXJqnzr8DJzE","redirect_uris":["http://marvin-assistant.herokuapp.com/?email=bob"],"javascript_origins":["http://marvin-assistant.herokuapp.com"]}}'
+let client_secret = '{"web":{"client_id":"696019611227-5m62hp5vit4ossvpv7pemo92prrlric8.apps.googleusercontent.com","project_id":"marvinassistantc-1512373709006","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://accounts.google.com/o/oauth2/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"Ce7jPpvZ7x4nUXJqnzr8DJzE","redirect_uris":["http://marvin-assistant.herokuapp.com/"],"javascript_origins":["http://marvin-assistant.herokuapp.com"]}}'
 
 //gets callback url to authorize user
 function getAuthorizationUrl(cb) {
